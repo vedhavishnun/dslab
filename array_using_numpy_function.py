d4e34d3db4007652993359ce85ebd9ece4516718
Vedha Vishnu <vedhavishnun@gmail.com>
2023-09-28 19:36:24 +0530

    added experminent 2and 3

--- array_using_numpy_function.py
+++ array_using_numpy_function.py
@@ -1,7 +1,8 @@
 #importing numpy commands
 import numpy as np
 np.random.seed(0)
-#here seed funtion is used for reproducabilty it will give same random number when we re run the program
+#here seed funtion is used for reproducabilty it will give same random number when we rerun the program
+#experiment 1 array indexing
 #single dimensional array
 x1=np.random.randint(10,size=6)
 print("single dimension array :",x1)
@@ -13,3 +14,12 @@ print("two dimensional array:",x2)
 print(x2[2,2])
 print(x2[0,1])
 print(x2[2,0])
+#experiment 2 slicing
+#single dimensional array
+print(x1[2:5])
+print(x1[2:])
+print(x1[:4])
+#two dimensional array
+print(x2[0:,2:])
+#here first index 0: refers to row and 2:refers to coloumn
+print(x2[:1,2:])
